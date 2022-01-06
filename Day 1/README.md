2dbfe20561a1527b6c92052f4db251036ee6f187
utplakshi <71188736+utplakshi@users.noreply.github.com>
2022-01-06 19:11:27 +0530

    06-01-2022
    Creating a variable and a function

---
+++ Day 1/README.md
@@ -0,0 +1,3 @@
+Learning about creating a variable and cretaing a function
+
+Create a function called HandleNameChange to randomly alot name change.
---
+++ Day 1/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const handleChangeName = () =>{
+     const name=['Utplakshi','Rob','Sakshi','Radha'];
+     const int= Math.floor(Math.random()*4);
+     return name[int];
+  }
+  return (
+   
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello {handleChangeName()}!!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
